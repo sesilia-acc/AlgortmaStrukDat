9a8c38e8ce7360be9a7ffa30f43003eb362d45a9
sesilia-acc <dandinsesilia@gmail.com>
2026-03-08 15:28:28 -0700

    percobaan 4 class

---
+++ jobsheet3/MataKuliah7.java
@@ -0,0 +1,13 @@
+public class MataKuliah7 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah7 (String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
