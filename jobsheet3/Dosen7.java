38dcf5c3b5e16be1be6e14d7df97b975530aa48f
sesilia-acc <dandinsesilia@gmail.com>
2026-03-08 19:47:40 -0700

    class tugas 1

---
+++ jobsheet3/Dosen7.java
@@ -0,0 +1,12 @@
+public class Dosen7 {
+    public String kode, nama;
+    public boolean jenisKelamin;
+    public int usia;
+
+    public Dosen7 (String kode, String nama, boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+}
