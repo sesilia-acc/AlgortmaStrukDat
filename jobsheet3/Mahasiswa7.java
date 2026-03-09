55af739c74255e4c16ba8e7025815cf6d5c1c670
sesilia-acc <dandinsesilia@gmail.com>
2026-03-08 19:48:47 -0700

    update

--- jobsheet3/Mahasiswa7.java
+++ jobsheet3/Mahasiswa7.java
@@ -4,8 +4,10 @@ public class Mahasiswa7 {
     public String kelas;
     public float ipk;
 
-    void cetakInfo(Mahasiswa7[] daftarMahasiswa7s) {
-        
-    
+    public void cetakInfo() {
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
     }
 } 
