981aa4a0a71dc9adca4f30077e36b7d21cdfab77
sesilia-acc <dandinsesilia@gmail.com>
2026-03-08 19:48:11 -0700

    update

--- jobsheet3/MataKuliahDemo7.java
+++ jobsheet3/MataKuliahDemo7.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class MataKuliahDemo7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliah7[] arrayOfMatakuliah7 = new MataKuliah7[3];
         String kode, nama, dummy;
-        int sks, jumlahJam;
+        int sks, jumlahJam, data;
+        System.out.println("Jumlah data yang ingin di input: ");
+        data = sc.nextInt();
+        sc.nextLine();
+
+        MataKuliah7[] arrayOfMatakuliah7 = new MataKuliah7[data];
 
         for (int i = 0; i < arrayOfMatakuliah7.length; i++) {
             System.out.println("Masukkan data Mata Kuliah ke-" + (i + 1));
@@ -15,13 +19,38 @@ public class MataKuliahDemo7 {
             System.out.print("Nama        : ");
             nama = sc.nextLine();
             System.out.print("SKS         : ");
-            sks = sc.nextInt();
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
             System.out.print("Jumlah Jam  : ");
             dummy = sc.nextLine();
             jumlahJam = Integer.parseInt(dummy);
-            System.out.println("----------------------------");
+            System.out.println("-------------------------------------------");
             
             arrayOfMatakuliah7[i] = new MataKuliah7(kode, nama, sks, jumlahJam);
         }
+        
+        System.out.print("Tambah data Mata Kuliah (ya/tidak): ");
+        String tambah = sc.nextLine();
+
+        if (tambah.equalsIgnoreCase("ya")) {
+            System.out.print("Kode        : ");
+            kode = sc.nextLine();
+            System.out.print("Nama        : ");
+            nama = sc.nextLine();
+            System.out.print("SKS         : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah jam  : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("----------------------------------------------");
+            arrayOfMatakuliah7[0].tambahData(kode, nama, sks, jumlahJam);
+        }
+        
+        for (int i = 0; i < arrayOfMatakuliah7.length; i++) {
+            System.out.println("Data Mata Kuliah ke-" + (i + 1));
+            arrayOfMatakuliah7[i].cetakInfo();
+        }
+        sc.close();
     }
 }
