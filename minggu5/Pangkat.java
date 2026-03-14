fa62e0e19ff29f373f4b6534d922b0bbd10e8bc0
sesilia-acc <dandinsesilia@gmail.com>
2026-03-14 11:59:51 -0800

    update class pangkat

--- minggu5/Pangkat.java
+++ minggu5/Pangkat.java
@@ -6,7 +6,7 @@ public class Pangkat {
         pangkat = p;
     }
 
-    int PangkatBF(int a, int n) {
+    int pangkatBF(int a, int n) {
         int hasil = 1;
         for (int i = 0; i < n; i++) {
             hasil *= a;
@@ -14,14 +14,14 @@ public class Pangkat {
         return hasil;
     }
 
-    int PangkatDC(int a, int n) {
+    int pangkatDC(int a, int n) {
         if (n == 1) {
             return a;
         } else {
             if (n % 2 == 1) {
-                return (PangkatDC(a, n / 2) * PangkatDC(a, n / 2) * a);
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2) * a);
             } else {
-                return (PangkatDC(a, n / 2) * PangkatDC(a, n / 2));
+                return (pangkatDC(a, n / 2) * pangkatDC(a, n / 2));
             }
         }
     }
