cbf243a33b5300df8c138c6905286a6abc81e3ff
sesilia-acc <dandinsesilia@gmail.com>
2026-03-14 09:57:20 -0800

    main pangkat

---
+++ minggu5/MainPangkat.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MainPangkat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = sc.nextInt();
+
+        Pangkat png[] = new Pangkat[elemen];
+        for (int i = 0; i < png.length; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-" + (i +1) + "     : ");
+            int basis = sc.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i + 1) + "   : ");
+            int pangkat =sc.nextInt();
+            png[i] = new Pangkat(basis, pangkat);
+        }
+
+        System.out.println("\n==== HASIL PANGKAT BRUTE FORCE ====");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.PangkatBF(p.nilai, p.pangkat));
+        }
+        System.out.println("\n==== HASIL PANGKAT DIVIDE AND CONQUER ====");
+        for (Pangkat p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.PangkatDC(p.nilai, p.pangkat));
+        }
+    }
+}
