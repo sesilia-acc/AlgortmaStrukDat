6e9cd8a27e1818ce6b73e99ea640b53601f7f7cd
sesilia-acc <dandinsesilia@gmail.com>
2026-04-11 19:21:22 -0800

    main bubble, selection, insertion sort

--- jobsheet6/SortingMain07.java
+++ jobsheet6/SortingMain07.java
@@ -1,13 +1,31 @@
 public class SortingMain07 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
-        Sorting07 dataurut1 =new Sorting07(a, a.length);
+        Sorting07 dataurut1 = new Sorting07(a, a.length);
+        Sorting07 dataurut2 = new Sorting07(b, b.length);
+        Sorting07 dataurut3 = new Sorting07(c, c.length);
 
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+        System.out.println();
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+        System.out.println();
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
