ae3f4c369d861b3d2849048fd596ac2e5b431153
sesilia-acc <dandinsesilia@gmail.com>
2026-04-12 10:47:48 -0800

    class mahasiswa

---
+++ jobsheet6/Mahasiswa07.java
@@ -0,0 +1,22 @@
+public class Mahasiswa07 {
+    String nim, nama, kelas;
+    double ipk;
+
+    public Mahasiswa07() {
+
+    }
+
+    public Mahasiswa07(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : " + nim);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("Kelas   : " + kelas);
+    }
+}
