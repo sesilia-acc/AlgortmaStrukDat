5a06579ed79cd37fd8609bd3c9c60f240b8a7eda
sesilia-acc <dandinsesilia@gmail.com>
2026-04-12 11:38:42 -0800

    modif class mapres

--- jobsheet6/MahasiswaBerprestasi07.java
+++ jobsheet6/MahasiswaBerprestasi07.java
@@ -1,7 +1,12 @@
 public class MahasiswaBerprestasi07 {
-    Mahasiswa07 listMhs[] = new Mahasiswa07[5];
+    Mahasiswa07 listMhs[];
     int idx;
-   
+
+    public MahasiswaBerprestasi07(int kuota) {
+        listMhs = new Mahasiswa07[kuota];
+        idx = 0;
+    }
+
     void tambah (Mahasiswa07 m) {
         if (idx < listMhs.length) {
             listMhs[idx] = m;
@@ -13,18 +18,22 @@ public class MahasiswaBerprestasi07 {
 
     void tampil() {
         for (Mahasiswa07 m : listMhs) {
-            m.tampilInformasi();
-            System.out.println("------------------------------");
+            if (m != null) {
+                m.tampilInformasi();
+                System.out.println("------------------------------");
+            }
         }
     }
 
     void bubbleSort() {
         for (int i = 0; i < listMhs.length - 1; i++) {
             for (int j = 1; j < listMhs.length - 1; j++) {
-                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
-                    Mahasiswa07 tmp = listMhs[j];
-                    listMhs[j] = listMhs[j - 1];
-                    listMhs[j - 1] = tmp;
+                if (listMhs[j] != null && listMhs[j - 1] != null) {
+                    if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                        Mahasiswa07 tmp = listMhs[j];
+                        listMhs[j] = listMhs[j - 1];
+                        listMhs[j - 1] = tmp;   
+                    }
                 }
             }
         }
