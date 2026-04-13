ee651c26ebf436a9c0bc3cbfa097b009629a3fc5
sesilia-acc <dandinsesilia@gmail.com>
2026-04-12 17:11:00 -0800

    class mapres insertion sort

--- jobsheet6/MahasiswaBerprestasi07.java
+++ jobsheet6/MahasiswaBerprestasi07.java
@@ -52,4 +52,16 @@ public class MahasiswaBerprestasi07 {
             listMhs[i] = temp;
         }
     }
+
+    void insertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa07 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j -1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
 }
