c8e8886cb8dd3512f719d0ef511524714d283cc6
sesilia-acc <dandinsesilia@gmail.com>
2026-05-01 18:52:32 -0800

    modif peek dan jumlah tugas

--- jobsheet9/MhsDemo7stack.java
+++ jobsheet9/MhsDemo7stack.java
@@ -14,17 +14,19 @@ public class MhsDemo7stack {
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
-            System.out.println("Pilih: ");
+            System.out.println("5. Melihat Tugas Terbawah");
+            System.out.println("6. Total Tugas");
+            System.out.print("Pilih: ");
             pilih = sc.nextInt();
             sc.nextLine();
 
             switch (pilih) {
                 case 1:
-                    System.out.print("Nama: ");
+                    System.out.print("Nama\t: ");
                     String nama = sc.nextLine();
-                    System.out.print("NIM: ");
+                    System.out.print("NIM\t: ");
                     String nim = sc.nextLine();
-                    System.out.print("Kelas: ");
+                    System.out.print("Kelas\t: ");
                     String kelas = sc.nextLine();
                     Mahasiswa7stack mhs = new Mahasiswa7stack(nama, nim, kelas, 0);
                     stack.push(mhs);
@@ -51,10 +53,25 @@ public class MhsDemo7stack {
                     System.out.println("Nama\t NIM\t Kelas");
                     stack.print();
                     break;
+                case 5:
+                    Mahasiswa7stack ketahui = stack.peek2();
+                    if (ketahui != null) {
+                        System.out.println("Tugas pertama kali dikumpulkan oleh " + ketahui.nama);
+                    }
+                    break;
+                case 6:
+                    int jmlTugas = stack.totalTugas();
+                    if (jmlTugas > 0) {
+                        System.out.println("Jumlah tugas saat ini adalah " + jmlTugas);
+                    } else {
+                        System.out.println("Tidak ada tugas yang dikumpulkan.");
+                    }
+                    break;
                 default:
-                    System.out.println("Pilihan tdak valid");
+                    System.out.println("Pilihan tidak valid");
             }
 
-        } while (pilih >= 1 && pilih <= 4);
+        } while (pilih >= 1 && pilih <= 6);
+        sc.close();
     }
 }
