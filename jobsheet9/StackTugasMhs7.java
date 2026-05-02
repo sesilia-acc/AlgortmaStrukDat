256a7351ef200a0abe9b7e192db9e7b34393bd2b
sesilia-acc <dandinsesilia@gmail.com>
2026-05-01 18:53:00 -0800

    modif class stack

--- jobsheet9/StackTugasMhs7.java
+++ jobsheet9/StackTugasMhs7.java
@@ -56,10 +56,27 @@ public class StackTugasMhs7 {
         }
     }
 
+    public Mahasiswa7stack peek2() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
     public void print() {
-        for (int i = 0; i < top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" +stack[i].kelas);
         }
         System.out.println("");
+    }
+
+    public int totalTugas() {
+        if (!isEmpty()) {
+            return top + 1;
+        } else {
+            return 0;
+        }
     }
 }
